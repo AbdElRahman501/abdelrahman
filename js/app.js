00a0985a45f0a7dc105e5db7ffe1e01315df61be
abdelrahman <abdelrahman.6182@gmail.com>
2022-12-02 01:16:53 +0200

    random shapes , content, navbar

--- js/app.js
+++ js/app.js
@@ -3,25 +3,42 @@
 // for shapes
 
 const shapes = document.getElementsByClassName("shape")
-console.log(shapes);
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < 7; i++) {
     let shipesList = document.getElementById("shapes").firstElementChild
     let shapes = document.getElementsByClassName("shape")
-    let newShape = document.createElement("li") 
+    let newShape = document.createElement("li")
     newShape.classList.add("shape")
     newShape.innerHTML = shapes[1].innerHTML
-    console.log(newShape);
-    shipesList.insertAdjacentElement("afterbegin",newShape )
-    
-}
-const cordinats = [{x:10 , y:10 ,w:3},{x:35 , y:70 ,w:13},{x:65 , y:5 ,w:15},{x:80 , y:45 ,w:5}]
+    shipesList.insertAdjacentElement("afterbegin", newShape)
 
+}
+const arrayTop = [];
+const arrayRight = [];
+for (let i = 1; i <= 85; i++) {
+    arrayTop.push(i);
+    arrayRight.push(i);
+}
 for (let i = 0; i < shapes.length; i++) {
     const shape = shapes[i];
-    let randomTop = cordinats[i].x
-    let randomRight = cordinats[i].y
-    let randomWidth = cordinats[i].w
-    shape.setAttribute('style', `position: absolute; top: ${randomTop}vh;right: ${randomRight}vw`);
+    let randomTop = getrandomnum(arrayTop);
+    let randomRight = getrandomnum(arrayRight);
+    let randomWidth = randomTop < 30 ? getRandomIntInclusive(70, 120) : getRandomIntInclusive(30, 50);
+    let blur = 50 / randomWidth
+    shape.setAttribute('style', `top: ${randomTop}vh;right: ${randomRight}vw`);
     let svg = shape.firstElementChild
-    svg.setAttribute('style', `width: ${randomWidth}rem;`)   
+    svg.setAttribute('style', `width: ${randomWidth}px; filter: blur(${blur}px) `)
+}
+function getrandomnum(array) {
+    let num = Math.floor(Math.random() * array.length);
+    let roll = array.splice(num - 1, 5);
+    let yourNumber = roll[2] 
+    return yourNumber
+}
+
+
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
