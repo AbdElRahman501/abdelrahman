e2e2b0fce85bfe45246094feb4f0501018303075
abdelrahman <abdelrahman.6182@gmail.com>
2022-12-02 04:10:50 +0200

    float animation,move shap animation , shake anim

--- js/app.js
+++ js/app.js
@@ -12,28 +12,50 @@ for (let i = 0; i < 7; i++) {
     shipesList.insertAdjacentElement("afterbegin", newShape)
 
 }
-const arrayTop = [];
-const arrayRight = [];
-for (let i = 1; i <= 85; i++) {
-    arrayTop.push(i);
-    arrayRight.push(i);
+let arrayTop = [];
+let arrayRight = [];
+const time = 15000
+
+function moveShapes() {
+    arrayTop = []
+    arrayRight = []
+    for (let i = 1; i <= 85; i++) {
+        arrayTop.push(i);
+        arrayRight.push(i);
+    }
+    for (let i = 0; i < shapes.length; i++) {
+        const shape = shapes[i];
+        let randomTop = getrandomnum(arrayTop, "top");
+        let randomRight = getrandomnum(arrayRight, "right");
+        let randomWidth = randomTop < 30 ? getRandomIntInclusive(70, 120) : getRandomIntInclusive(30, 50);
+        let blur = 50 / randomWidth
+        // console.log({ randomTop, randomRight, randomWidth })
+        shape.setAttribute('style', `top: ${randomTop}vh;right: ${randomRight}vw`);
+        let svg = shape.firstElementChild
+        svg.setAttribute('style', `width: ${randomWidth}px; filter: blur(${blur}px) `)
+    }
+    function getrandomnum(array, type) {
+        let num = Math.floor(Math.random() * array.length);
+        let roll = array.splice(num - 1, 10);
+        let yourNumber = roll.length % 2 === 0 ? roll[roll.length / 2] : roll[(roll.length - 1) / 2];
+        // console.log(yourNumber, roll, array, type, roll.length % 2 === 0 ? roll.length / 2 : (roll.length - 1) / 2)
+        return yourNumber;
+    }
+    setTimeout(shake,time - 2000)
 }
-for (let i = 0; i < shapes.length; i++) {
-    const shape = shapes[i];
-    let randomTop = getrandomnum(arrayTop);
-    let randomRight = getrandomnum(arrayRight);
-    let randomWidth = randomTop < 30 ? getRandomIntInclusive(70, 120) : getRandomIntInclusive(30, 50);
-    let blur = 50 / randomWidth
-    shape.setAttribute('style', `top: ${randomTop}vh;right: ${randomRight}vw`);
-    let svg = shape.firstElementChild
-    svg.setAttribute('style', `width: ${randomWidth}px; filter: blur(${blur}px) `)
+function shake() {
+    for (const shape of shapes) {
+        shape.classList.add("shake")
+        setTimeout(() => {
+            shape.classList.remove("shake")
+        }, 2000)
+    }
 }
-function getrandomnum(array) {
-    let num = Math.floor(Math.random() * array.length);
-    let roll = array.splice(num - 1, 5);
-    let yourNumber = roll[2] 
-    return yourNumber
-}
+
+moveShapes()
+setInterval(moveShapes, time)
+
+
 
 
 
